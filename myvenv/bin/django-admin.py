2ca56b0e4b3524880d99a1d948b6c4d1002459b2
sushmanthreddy <amenu4ece20134@am.students.amrita.edu>
2021-06-04 15:54:09 +0530

    backend changes

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/sushmanthreddy/sushmanthreddy/Blog-and-Portfolio/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
